31a8d9005fdd8d290b3ff578d6873d44d8ee2859
nancy lopez <nancylopezgomez70@gmail.com>
2023-08-15 22:29:47 -0600

    segunda parte y resolucion de la practica programada 3

--- app tarea.R
+++ app tarea.R
@@ -27,4 +27,44 @@ ui <- fluidPage(
                   choices = c("San José", "Heredia", "Cartago", "Limón", "Puntarenas", "Alajuela"),
                   multiple = FALSE)
  
+    ), 
+    mainPanel(
+      width = 7,
+      br(),
+      br(),
+      h3(div("¡Vamos a planificar su día especial!", style = "color:blue", align = "center")),
+      tags$hr(style = "border-color:blue;"), 
+      textOutput("evento_output"), 
+      br(),
+      tags$hr(),
+      textOutput("tema_output"),
+      br(),
+      tags$hr(),
+      textOutput("invitados_provincia_output")
+    )
+  )
+)
 
+server <- function(input, output) {
+  output$evento_output <- renderText({
+    evento <- input$nombre
+    paste("Evento:", evento)
+  })
+  
+  output$tema_output <- renderText({
+    tema <- input$tema
+    paste("Tema del Evento:", tema)
+  })
+  
+  output$invitados_provincia_output <- renderText({
+    tema <- input$tema
+    invitados <- input$invitados
+    provincia <- input$provincia
+    paste("Número de invitados y ubicación: El evento '", tema, "' tendrá ", invitados, " invitados y será llevado a cabo en ", provincia, ".")
+  })
+}
+
+shinyApp(ui, server)
+
+    
+    
